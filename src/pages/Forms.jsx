da9b7fed585af70c46aaa479281281058ca6a679
Noman Dipto <noman40046@gmail.com>
2024-05-08 17:37:05 -0700

    form solve

--- src/pages/Forms.jsx
+++ src/pages/Forms.jsx
@@ -156,263 +156,268 @@ const Forms = () => {
                       <div className="form-control">
                         <label className="label cursor-pointer ">
                           <div class="item-detail ">
-                              <div>
-                                <img className="car" src="../../public/images/s5.png" alt="" />
-                              </div>
-                              <div>
-                                <h2 className="sedan">Minibus</h2>
-                                <p className="car-seat">4 Seats</p>
-                              </div>
+                            <div>
+                              <img
+                                className="car"
+                                src="../../public/images/s5.png"
+                                alt=""
+                              />
                             </div>
-                            <input
-                              type="radio"
-                              name="radio-10"
-                              className="radio checked:bg-red-500 border-gray-400"
-                              checked
-                            />
-                          </label>
-                        </div>
-  
+                            <div>
+                              <h2 className="sedan">Minibus</h2>
+                              <p className="car-seat">4 Seats</p>
+                            </div>
+                          </div>
+                          <input
+                            type="radio"
+                            name="radio-10"
+                            className="radio checked:bg-red-500 border-gray-400"
+                            checked
+                          />
+                        </label>
                       </div>
                     </div>
-                  </dialog>
-                </div>
+                  </div>
+                </dialog>
               </div>
-  
-              <div className="devider"></div>
-  
-              <div class="destination flex gap-4 items-center px-10  py-3">
-                <div class="pick-drop flex flex-col gap-3 items-center">
-                  <span class="pickup-logo material-symbols-outlined">
-                    radio_button_unchecked
-                  </span>
-                  <div className="vertile"></div>
-                  {/*  */}
-                  <span
-                    id="hiddenDiv3"
-                    className="hidden pickup-logo2 material-symbols-outlined"
-                  >
-                    radio_button_unchecked
-                  </span>
-                  <div id="hiddenDiv4" className="hidden vertile"></div>
-                  {/*  */}
-  
-                  <span class="drop-logo material-symbols-outlined">stop</span>
+            </div>
+
+            <div className="devider"></div>
+
+            <div class="destination flex gap-4 items-center px-10  py-3">
+              <div class="pick-drop flex flex-col gap-3 items-center">
+                <span class="pickup-logo material-symbols-outlined">
+                  radio_button_unchecked
+                </span>
+                <div className="vertile"></div>
+                {/*  */}
+                <span
+                  id="hiddenDiv3"
+                  className="hidden pickup-logo2 material-symbols-outlined"
+                >
+                  radio_button_unchecked
+                </span>
+                <div id="hiddenDiv4" className="hidden vertile"></div>
+                {/*  */}
+
+                <span class="drop-logo material-symbols-outlined">stop</span>
+              </div>
+
+              <div className="flex flex-col gap-5 me-20">
+                <div>
+                  <h2 className="pickup">Pickup Point</h2>
+                  <input
+                    placeholder="Search pickup location"
+                    className="search focus:outline-none"
+                    type="search"
+                  />
                 </div>
-  
-                <div className="flex flex-col gap-5 me-20">
-                  <div>
-                    <h2 className="pickup">Pickup Point</h2>
-                    <input
-                      placeholder="Search pickup location"
-                      className="search focus:outline-none"
-                      type="search"
-                    />
-                  </div>
-  
-                  <div className="relative" data-v-a0ca3c8a>
-                    <hr class="border-dashed mt-[4px] w-[470px]" />
-                    <img
-                      id="toggleIcon"
-                      src="https://rental.jatri.co/_nuxt/add-via-point.68721e6d.svg"
-                      className="w-[30px] h-[30px] absolute -right-[10px] -top-[13px] transition-all ease-in-out cursor-pointer "
-                      alt
-                      data-v-a0ca3c8a
-                    />
-                  </div>
-  
-                  {/*  */}
-                  <div id="hiddenDiv5" className="hidden">
-                    <h2 className="pickup ">Via Point</h2>
-                    <input
-                      placeholder="Search via location "
-                      className="search focus:outline-none"
-                      type="search"
-                    />
-                  </div>
-  
-                  <div
-                    id="hiddenDiv6"
-                    className="hidden relative"
+
+                <div className="relative" data-v-a0ca3c8a>
+                  <hr class="border-dashed mt-[4px] w-[470px]" />
+                  {/* <img
+                    id="toggleIcon"
+                    src="https://rental.jatri.co/_nuxt/add-via-point.68721e6d.svg"
+                    className="w-[30px] h-[30px] absolute -right-[10px] -top-[13px] transition-all ease-in-out cursor-pointer "
+                    alt
                     data-v-a0ca3c8a
-                  >
-                    <hr class="border-dashed mt-[4px] w-[470px]" />
-                  </div>
-                  {/*  */}
-  
-                  <div>
-                    <h2 className="pickup">Drop Off Point</h2>
-                    <input
-                      placeholder="Search drop off location"
-                      className="search focus:outline-none"
-                      type="search"
-                    />
-                  </div>
+                  /> */}
                 </div>
-              </div>
-  
-              <div className="devider"></div>
-  
-              <div class="date-time flex justify-between items-center px-10  py-6">
-                <div class="item-detail ">
-                  <div className="flex gap-2">
-                    <div>
-                      <a href="#">
-                        <span class="schedule-logo material-symbols-outlined">
-                          calendar_month
-                        </span>
-                      </a>
-                    </div>
-                    <div>
-                      <h1 className="dt">Select Date</h1>
-                    </div>
-                  </div>
-                </div>
-                <div>
+
+
+                {/*  */}
+             {/*    <div id="hiddenDiv5" className="hidden">
+                  <h2 className="pickup ">Via Point</h2>
                   <input
-                    type="date"
-                    placeholder="Type here"
-                    className="dtt input  w-full max-w-xs "
+                    placeholder="Search via location "
+                    className="search focus:outline-none"
+                    type="search"
                   />
                 </div>
-              </div>
-  
-              <hr />
-  
-              <div class="date-time flex justify-between items-center px-10  py-6">
-                <div class="item-detail ">
-                  <div className="flex gap-2">
-                    <div>
-                      <a href="#">
-                        <span class="schedule-logo material-symbols-outlined">
-                          schedule
-                        </span>
-                      </a>
-                    </div>
-                    <div>
-                      <h1 className="dt">Select Time</h1>
-                    </div>
-                  </div>
-                </div>
-                <div>
-                  <input
-                    type="time"
-                    placeholder="3.44 am"
-                    className="dtt dtt2 input  bg-white  w-full max-w-xs "
-                  />
-                </div>
-              </div>
-  
-              <hr />
-  
-              <div class="date-time flex justify-between items-center px-10  py-6">
-                <div class="item-detail ">
-                  <div className="flex gap-2">
-                    <div>
-                      <a href="#">
-                        <span class=" schedule-logo2 material-symbols-outlined">
-                          travel
-                        </span>
-                      </a>
-                    </div>
-                    <div>
-                      <h1 className="dt">Round Trip </h1>
-                    </div>
-                  </div>
-                </div>
-                <div>
-                  <label for="toggle" class="flex items-center cursor-pointer">
-                    <div class="relative">
-                      <input id="toggle" type="checkbox" class="hidden" />
-  
-                      <div class="toggle-line w-10 h-5 bg-gray-400 rounded-full shadow-inner"></div>
-  
-                      <div class="toggle-dot absolute w-5 h-5 bg-white rounded-full shadow inset-y-0 left-0"></div>
-                    </div>
-                  </label>
-                </div>
-              </div>
-  
-              <hr />
-  
-              <div
-                  id="hiddenDiv1"
-                  class="flex  date-time justify-between items-center px-10  py-6"
-                >
-                  <div class="item-detail ">
-                    <div className="flex gap-2">
-                      <div>
-                        <a href="#">
-                          <span class="schedule-logo material-symbols-outlined">
-                            calendar_month
-                          </span>
-                        </a>
-                      </div>
-                      <div>
-                        <h1 className="dt"> Select Round Trip Date</h1>
-                      </div>
-                    </div>
-                  </div>
-                  <div>
-                    <input
-                      type="date"
-                      placeholder="Type here"
-                      className="dtt input  w-full max-w-xs "
-                    />
-                  </div>
-                </div>
-                <hr />
+
                 <div
-                  id="hiddenDiv2"
-                  class="flex  date-time justify-between items-center px-10  py-6"
+                  id="hiddenDiv6"
+                  className="hidden relative"
+                  data-v-a0ca3c8a
                 >
-                  <div class="item-detail ">
-                    <div className="flex gap-2">
-                      <div>
-                        <a href="#">
-                          <span class="schedule-logo material-symbols-outlined">
-                            schedule
-                          </span>
-                        </a>
-                      </div>
-                      <div>
-                        <h1 className="dt">Select Time</h1>
-                      </div>
-                    </div>
+                  <hr class="border-dashed mt-[4px] w-[470px]" />
+                </div> */}
+                {/*  */}
+
+
+                <div>
+                  <h2 className="pickup">Drop Off Point</h2>
+                  <input
+                    placeholder="Search drop off location"
+                    className="search focus:outline-none"
+                    type="search"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="devider"></div>
+
+            <div class="date-time flex justify-between items-center px-10  py-6">
+              <div class="item-detail ">
+                <div className="flex gap-2">
+                  <div>
+                    <a href="#">
+                      <span class="schedule-logo material-symbols-outlined">
+                        calendar_month
+                      </span>
+                    </a>
                   </div>
                   <div>
-                    <input
-                      type="time"
-                      placeholder="3.44 am"
-                      className="dtt dtt2 input  bg-white  w-full max-w-xs "
-                    />
+                    <h1 className="dt">Select Date</h1>
                   </div>
                 </div>
-              </>
-            )}
+              </div>
+              <div>
+                <input
+                  type="date"
+                  placeholder="Type here"
+                  className="dtt input  w-full max-w-xs "
+                />
+              </div>
+            </div>
 
             <hr />
 
-
-            
-
-            <div class="flex  justify-center py-6">
+            <div class="date-time flex justify-between items-center px-10  py-6">
               <div class="item-detail ">
-                <form>
-                  <textarea
-                    className="messege"
-                    type="text"
-                    maxlength="100"
-                    minlength="10"
-                    placeholder="Enter your name"
-                  />
-                </form>
+                <div className="flex gap-2">
+                  <div>
+                    <a href="#">
+                      <span class="schedule-logo material-symbols-outlined">
+                        schedule
+                      </span>
+                    </a>
+                  </div>
+                  <div>
+                    <h1 className="dt">Select Time</h1>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <input
+                  type="time"
+                  placeholder="3.44 am"
+                  className="dtt dtt2 input  bg-white  w-full max-w-xs "
+                />
               </div>
             </div>
 
-            <div className="flex justify-center">
-              <button className="form-btn btn btn-secondary px-20">Next</button>
+            <hr />
+
+            <div class="date-time flex justify-between items-center px-10  py-6">
+              <div class="item-detail ">
+                <div className="flex gap-2">
+                  <div>
+                    <a href="#">
+                      <span class=" schedule-logo2 material-symbols-outlined">
+                        travel
+                      </span>
+                    </a>
+                  </div>
+                  <div>
+                    <h1 className="dt">Round Trip </h1>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <button onClick={handleToggle}>
+                  {isToggled ? (
+                    <FaToggleOn size={30} />
+                  ) : (
+                    <FaToggleOff size={30} />
+                  )}
+                </button>
+              </div>
             </div>
+
+            <hr />
+
+            <div>
+              {isToggled && (
+                <div>
+                  <div
+                    id="hiddenDiv1"
+                    class="flex  date-time justify-between items-center px-10  py-6"
+                  >
+                    <div class="item-detail ">
+                      <div className="flex gap-2">
+                        <div>
+                          <a href="#">
+                            <span class="schedule-logo material-symbols-outlined">
+                              calendar_month
+                            </span>
+                          </a>
+                        </div>
+                        <div>
+                          <h1 className="dt"> Select Round Trip Date</h1>
+                        </div>
+                      </div>
+                    </div>
+                    <div>
+                      <input
+                        type="date"
+                        placeholder="Type here"
+                        className="dtt input  w-full max-w-xs "
+                      />
+                    </div>
+                  </div>
+
+                  <hr />
+                  <div
+                    id="hiddenDiv2"
+                    class="flex  date-time justify-between items-center px-10  py-6"
+                  >
+                    <div class="item-detail ">
+                      <div className="flex gap-2">
+                        <div>
+                          <a href="#">
+                            <span class="schedule-logo material-symbols-outlined">
+                              schedule
+                            </span>
+                          </a>
+                        </div>
+                        <div>
+                          <h1 className="dt">Select Time</h1>
+                        </div>
+                      </div>
+                    </div>
+                    <div>
+                      <input
+                        type="time"
+                        placeholder="3.44 am"
+                        className="dtt dtt2 input  bg-white  w-full max-w-xs "
+                      />
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+
+          <hr />
+
+          <div class="flex  justify-center py-6">
+            <div class="item-detail ">
+              <form>
+                <textarea
+                  className="messege"
+                  type="text"
+                  maxlength="100"
+                  minlength="10"
+                  placeholder="Enter your name"
+                />
+              </form>
+            </div>
+          </div>
+
+          <div className="flex justify-center">
+            <button className="form-btn btn btn-secondary px-20">Next</button>
           </div>
         </div>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,15 @@
-import React, { useState } from "react";
-import { FaToggleOn, FaToggleOff } from "react-icons/fa";
+import React, from "react";
+
 
 const Home = () => {
-  const [isToggled, setIsToggled] = useState(false);
+  
 
-  const handleToggle = () => {
-    setIsToggled(!isToggled);
-  };
   return (
     <div>
       <h1 className="text-center text-xl font-bold">
         This is Home/Zatri section
       </h1>
-      <div>
-        <button onClick={handleToggle}>
-          {isToggled ? <FaToggleOn size={30} /> : <FaToggleOff size={30} />}
-        </button>
-        {isToggled && <div>The toggle is on!</div>}
-      </div>
+    
     </div>
   );
 };
