7c2cbd4efc86359bad0f5333d2fa66d24c20fe1f
Noman Dipto <noman40046@gmail.com>
2024-05-11 12:14:41 -0700

    form location

---
+++ src/components/Carinfo.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+const Carinfo = () => {
+    return (
+        <div>
+                <div className="mt-24 mb-10 mx-12">
+        <h1 className="mb-10 text-4xl font-bold">
+          Rent a vehicle that fit your budget
+        </h1>
+        <div className="flex justify-between">
+          <div className="w-[300px]">
+            <img src="../../public/images/s1.png" alt="" />
+            <h2 className="text-xl font-semibold">Enter your travel details</h2>
+            <p>
+              Enter your pickup, destination and trip details to place a trip
+              request
+            </p>
+          </div>
+          <div className="w-[300px]">
+            <img src="../../public/images/s2.png" alt="" />
+            <h2 className="text-xl font-semibold">Browse Through Bits</h2>
+            <p>
+              Enter your pickup, destination and trip details to place a trip
+              request
+            </p>
+          </div>
+          <div className="w-[300px]">
+            <img src="../../public/images/s3.png" alt="" />
+            <h2 className="text-xl font-semibold">Confirm Your Trip</h2>
+            <p>
+              Enter your pickup, destination and trip details to place a trip
+              request
+            </p>
+          </div>
+        </div>
+        {/* 2nd ----------- */}
+        <div className="flex justify-between">
+          <div className="w-[300px]">
+            <img src="../../public/images/s4.png" alt="" />
+            <h2 className="text-xl font-semibold">Enter your travel details</h2>
+            <p>
+              Enter your pickup, destination and trip details to place a trip
+              request
+            </p>
+          </div>
+          <div className="w-[300px]">
+            <img src="../../public/images/s5.png" alt="" />
+            <h2 className="text-xl font-semibold">Browse Through Bits</h2>
+            <p>
+              Enter your pickup, destination and trip details to place a trip
+              request
+            </p>
+          </div>
+          <div className="w-[300px]">
+            <img src="../../public/images/s6.png" alt="" />
+            <h2 className="text-xl font-semibold">Confirm Your Trip</h2>
+            <p>
+              Enter your pickup, destination and trip details to place a trip
+              request
+            </p>
+          </div>
+        </div>
+      </div>
+        </div>
+    );
+};
+
+export default Carinfo;
